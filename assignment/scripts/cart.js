f1a6e852f13ad0c1ce7b30a91f345c00a1591a5a
Rickie Natividad <rickienatividad85@gmail.com>
2022-05-26 15:51:56 -0500

    addItem function completed and working

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,17 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem(item){
+    basket.push(item);
+    return true;
+}
+console.log(`it's`,addItem('eggs'),`eggs were added to the basket`);
+console.log(`basket has:`,basket);
+addItem('milk');
+addItem('oatmeal');
+console.log(basket);
+
+function listItems(array){
+    
+}
