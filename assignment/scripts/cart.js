e451fd76bb60f347e413cafcae3591bdb3c309d7
Rickie Natividad <rickienatividad85@gmail.com>
2022-05-26 16:45:58 -0500

    listItems and empty functions both completed

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,14 +4,29 @@ console.log('***** Cart Functions *****');
 let basket = [];
 function addItem(item){
     basket.push(item);
-    return true;
+    if(item){
+        return true;
+    }else{
+        return false;
+    }
 }
 console.log(`it's`,addItem('eggs'),`eggs were added to the basket`);
-console.log(`basket has:`,basket);
+// console.log(`basket has:`,basket);
 addItem('milk');
 addItem('oatmeal');
 console.log(basket);
 
 function listItems(array){
-    
+    for(let items of array){
+        console.log(items);
+    }
 }
+listItems(basket);
+
+function empty(array){
+    while(array.length > 0){
+        array.pop();
+    }
+}
+empty(basket);
+console.log(basket);
